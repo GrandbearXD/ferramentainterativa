723c98516a76bb945067a7f252edea77c56b3bce
GrandbearXD <145041120+GrandbearXD@users.noreply.github.com>
2024-11-21 19:27:13 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'O que é um array?',
+    'É uma estrutura de dados que armazena vários valores em uma única variável'
 )
 
 criaCartao(
     'Programação',
     'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Um bloco de código reutilizável'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa "debugar" um programa?',
+    'Significa encontrar e corrigir erros ou bugs no código para que ele funcione corretamente'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'O que é um loop?',
+    'Uma estrutura que repete um bloco de código'
 )
